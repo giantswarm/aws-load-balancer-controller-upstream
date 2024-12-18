kubernetes-sigs/aws-load-balancer-controller

propagate additional tags to security group created by the operator

pkg/ingress/model_build_load_balancer.go
@@ -52,7 +52,11 @@ func (t *defaultModelBuildTask) buildLoadBalancerSpec(ctx context.Context, liste
 	if err != nil {
 		return elbv2model.LoadBalancerSpec{}, err
 	}
-	securityGroups, err := t.buildLoadBalancerSecurityGroups(ctx, listenPortConfigByPort, ipAddressType)
+	tags, err := t.buildLoadBalancerTags(ctx)
+	if err != nil {
+		return elbv2model.LoadBalancerSpec{}, err
+	}
+	securityGroups, err := t.buildLoadBalancerSecurityGroups(ctx, listenPortConfigByPort, ipAddressType, tags)
 	if err != nil {
 		return elbv2model.LoadBalancerSpec{}, err
 	}
@@ -61,10 +65,6 @@ func (t *defaultModelBuildTask) buildLoadBalancerSpec(ctx context.Context, liste
 		return elbv2model.LoadBalancerSpec{}, err
 	}
 	loadBalancerAttributes, err := t.buildLoadBalancerAttributes(ctx)
-	if err != nil {
-		return elbv2model.LoadBalancerSpec{}, err
-	}
-	tags, err := t.buildLoadBalancerTags(ctx)
 	if err != nil {
 		return elbv2model.LoadBalancerSpec{}, err
 	}
@@ -270,7 +270,7 @@ func (t *defaultModelBuildTask) buildLoadBalancerSubnetMappings(ctx context.Cont
 	return buildLoadBalancerSubnetMappingsWithSubnetIDs(subnetIDs), nil
 }
 
-func (t *defaultModelBuildTask) buildLoadBalancerSecurityGroups(ctx context.Context, listenPortConfigByPort map[int64]listenPortConfig, ipAddressType elbv2model.IPAddressType) ([]core.StringToken, error) {
+func (t *defaultModelBuildTask) buildLoadBalancerSecurityGroups(ctx context.Context, listenPortConfigByPort map[int64]listenPortConfig, ipAddressType elbv2model.IPAddressType, additionalTags map[string]string) ([]core.StringToken, error) {
 	sgNameOrIDsViaAnnotation, err := t.buildFrontendSGNameOrIDsFromAnnotation(ctx)
 	if err != nil {
 		return nil, err
@@ -285,7 +285,7 @@ func (t *defaultModelBuildTask) buildLoadBalancerSecurityGroups(ctx context.Cont
 		if !t.enableBackendSG {
 			t.backendSGIDToken = managedSG.GroupID()
 		} else {
-			backendSGID, err := t.backendSGProvider.Get(ctx, networking.ResourceTypeIngress, k8s.ToSliceOfNamespacedNames(t.ingGroup.Members))
+			backendSGID, err := t.backendSGProvider.Get(ctx, networking.ResourceTypeIngress, k8s.ToSliceOfNamespacedNames(t.ingGroup.Members), additionalTags)
 			if err != nil {
 				return nil, err
 			}
@@ -311,7 +311,7 @@ func (t *defaultModelBuildTask) buildLoadBalancerSecurityGroups(ctx context.Cont
 			if !t.enableBackendSG {
 				return nil, errors.New("backendSG feature is required to manage worker node SG rules when frontendSG manually specified")
 			}
-			backendSGID, err := t.backendSGProvider.Get(ctx, networking.ResourceTypeIngress, k8s.ToSliceOfNamespacedNames(t.ingGroup.Members))
+			backendSGID, err := t.backendSGProvider.Get(ctx, networking.ResourceTypeIngress, k8s.ToSliceOfNamespacedNames(t.ingGroup.Members), additionalTags)
 			if err != nil {
 				return nil, err
 			}

pkg/networking/backend_sg_provider.go
@@ -51,7 +51,7 @@ const (
 // BackendSGProvider is responsible for providing backend security groups
 type BackendSGProvider interface {
 	// Get returns the backend security group to use
-	Get(ctx context.Context, resourceType ResourceType, activeResources []types.NamespacedName) (string, error)
+	Get(ctx context.Context, resourceType ResourceType, activeResources []types.NamespacedName, additionalTags map[string]string) (string, error)
 	// Release cleans up the auto-generated backend SG if necessary
 	Release(ctx context.Context, resourceType ResourceType, inactiveResources []types.NamespacedName) error
 }
@@ -119,12 +119,12 @@ type defaultBackendSGProvider struct {
 	defaultDeletionTimeout      time.Duration
 }
 
-func (p *defaultBackendSGProvider) Get(ctx context.Context, resourceType ResourceType, activeResources []types.NamespacedName) (string, error) {
+func (p *defaultBackendSGProvider) Get(ctx context.Context, resourceType ResourceType, activeResources []types.NamespacedName, additionalTags map[string]string) (string, error) {
 	if len(p.backendSG) > 0 {
 		return p.backendSG, nil
 	}
 	// Auto generate Backend Security group, and return the id
-	if err := p.allocateBackendSG(ctx, resourceType, activeResources); err != nil {
+	if err := p.allocateBackendSG(ctx, resourceType, activeResources, additionalTags); err != nil {
 		p.logger.Error(err, "Failed to auto-create backend SG")
 		return "", err
 	}
@@ -216,7 +216,7 @@ func (p *defaultBackendSGProvider) existsInObjectMap(resourceType ResourceType, 
 	return false
 }
 
-func (p *defaultBackendSGProvider) allocateBackendSG(ctx context.Context, resourceType ResourceType, activeResources []types.NamespacedName) error {
+func (p *defaultBackendSGProvider) allocateBackendSG(ctx context.Context, resourceType ResourceType, activeResources []types.NamespacedName, additionalTags map[string]string) error {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
 
@@ -240,7 +240,7 @@ func (p *defaultBackendSGProvider) allocateBackendSG(ctx context.Context, resour
 		VpcId:             awssdk.String(p.vpcID),
 		GroupName:         awssdk.String(sgName),
 		Description:       awssdk.String(sgDescription),
-		TagSpecifications: p.buildBackendSGTags(ctx),
+		TagSpecifications: p.buildBackendSGTags(ctx, additionalTags),
 	}
 	p.logger.V(1).Info("creating securityGroup", "name", sgName)
 	resp, err := p.ec2Client.CreateSecurityGroupWithContext(ctx, createReq)
@@ -252,21 +252,29 @@ func (p *defaultBackendSGProvider) allocateBackendSG(ctx context.Context, resour
 	return nil
 }
 
-func (p *defaultBackendSGProvider) buildBackendSGTags(_ context.Context) []*ec2sdk.TagSpecification {
-	var defaultTags []*ec2sdk.Tag
+func (p *defaultBackendSGProvider) buildBackendSGTags(_ context.Context, additionalTags map[string]string) []*ec2sdk.TagSpecification {
+	var tags []*ec2sdk.Tag
 	for key, val := range p.defaultTags {
-		defaultTags = append(defaultTags, &ec2sdk.Tag{
+		tags = append(tags, &ec2sdk.Tag{
 			Key:   awssdk.String(key),
 			Value: awssdk.String(val),
 		})
 	}
-	sort.Slice(defaultTags, func(i, j int) bool {
-		return awssdk.StringValue(defaultTags[i].Key) < awssdk.StringValue(defaultTags[j].Key)
+
+	for key, val := range additionalTags {
+		tags = append(tags, &ec2sdk.Tag{
+			Key:   awssdk.String(key),
+			Value: awssdk.String(val),
+		})
+	}
+
+	sort.Slice(tags, func(i, j int) bool {
+		return awssdk.StringValue(tags[i].Key) < awssdk.StringValue(tags[j].Key)
 	})
 	return []*ec2sdk.TagSpecification{
 		{
 			ResourceType: awssdk.String(resourceTypeSecurityGroup),
-			Tags: append(defaultTags, []*ec2sdk.Tag{
+			Tags: append(tags, []*ec2sdk.Tag{
 				{
 					Key:   awssdk.String(tagKeyK8sCluster),
 					Value: awssdk.String(p.clusterName),

pkg/service/model_build_load_balancer.go
@@ -57,11 +57,11 @@ func (t *defaultModelBuildTask) buildLoadBalancerSpec(ctx context.Context, schem
 	if err != nil {
 		return elbv2model.LoadBalancerSpec{}, err
 	}
-	securityGropus, err := t.buildLoadBalancerSecurityGroups(ctx, existingLB, ipAddressType)
+	tags, err := t.buildLoadBalancerTags(ctx)
 	if err != nil {
 		return elbv2model.LoadBalancerSpec{}, err
 	}
-	tags, err := t.buildLoadBalancerTags(ctx)
+	securityGropus, err := t.buildLoadBalancerSecurityGroups(ctx, existingLB, ipAddressType, tags)
 	if err != nil {
 		return elbv2model.LoadBalancerSpec{}, err
 	}
@@ -87,7 +87,7 @@ func (t *defaultModelBuildTask) buildLoadBalancerSpec(ctx context.Context, schem
 }
 
 func (t *defaultModelBuildTask) buildLoadBalancerSecurityGroups(ctx context.Context, existingLB *elbv2deploy.LoadBalancerWithTags,
-	ipAddressType elbv2model.IPAddressType) ([]core.StringToken, error) {
+	ipAddressType elbv2model.IPAddressType, tags map[string]string) ([]core.StringToken, error) {
 	if existingLB != nil && len(existingLB.LoadBalancer.SecurityGroups) == 0 {
 		return nil, nil
 	}
@@ -109,7 +109,7 @@ func (t *defaultModelBuildTask) buildLoadBalancerSecurityGroups(ctx context.Cont
 		if !t.enableBackendSG {
 			t.backendSGIDToken = managedSG.GroupID()
 		} else {
-			backendSGID, err := t.backendSGProvider.Get(ctx, networking.ResourceTypeService, []types.NamespacedName{k8s.NamespacedName(t.service)})
+			backendSGID, err := t.backendSGProvider.Get(ctx, networking.ResourceTypeService, []types.NamespacedName{k8s.NamespacedName(t.service)}, tags)
 			if err != nil {
 				return nil, err
 			}
@@ -133,7 +133,7 @@ func (t *defaultModelBuildTask) buildLoadBalancerSecurityGroups(ctx context.Cont
 			if !t.enableBackendSG {
 				return nil, errors.New("backendSG feature is required to manage worker node SG rules when frontendSG is manually specified")
 			}
-			backendSGID, err := t.backendSGProvider.Get(ctx, networking.ResourceTypeService, []types.NamespacedName{k8s.NamespacedName(t.service)})
+			backendSGID, err := t.backendSGProvider.Get(ctx, networking.ResourceTypeService, []types.NamespacedName{k8s.NamespacedName(t.service)}, tags)
 			if err != nil {
 				return nil, err
 			}

giantswarm/aws-load-balancer-controller-upstream

pkg/ingress/model_build_load_balancer.go
@@ -5,12 +5,13 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"regexp"
+
 	awssdk "github.com/aws/aws-sdk-go-v2/aws"
 	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/google/go-cmp/cmp"
 	"github.com/pkg/errors"
 	"k8s.io/apimachinery/pkg/util/sets"
-	"regexp"
 	"sigs.k8s.io/aws-load-balancer-controller/apis/elbv2/v1beta1"
 	"sigs.k8s.io/aws-load-balancer-controller/pkg/algorithm"
 	"sigs.k8s.io/aws-load-balancer-controller/pkg/annotations"
@@ -51,7 +52,11 @@ func (t *defaultModelBuildTask) buildLoadBalancerSpec(ctx context.Context, liste
 	if err != nil {
 		return elbv2model.LoadBalancerSpec{}, err
 	}
-	securityGroups, err := t.buildLoadBalancerSecurityGroups(ctx, listenPortConfigByPort, ipAddressType)
+	tags, err := t.buildLoadBalancerTags(ctx)
+	if err != nil {
+		return elbv2model.LoadBalancerSpec{}, err
+	}
+	securityGroups, err := t.buildLoadBalancerSecurityGroups(ctx, listenPortConfigByPort, ipAddressType, tags)
 	if err != nil {
 		return elbv2model.LoadBalancerSpec{}, err
 	}
@@ -60,10 +65,6 @@ func (t *defaultModelBuildTask) buildLoadBalancerSpec(ctx context.Context, liste
 		return elbv2model.LoadBalancerSpec{}, err
 	}
 	loadBalancerAttributes, err := t.buildLoadBalancerAttributes(ctx)
-	if err != nil {
-		return elbv2model.LoadBalancerSpec{}, err
-	}
-	tags, err := t.buildLoadBalancerTags(ctx)
 	if err != nil {
 		return elbv2model.LoadBalancerSpec{}, err
 	}
@@ -276,7 +277,7 @@ func (t *defaultModelBuildTask) buildLoadBalancerSubnetMappings(ctx context.Cont
 	return buildLoadBalancerSubnetMappingsWithSubnetIDs(subnetIDs), nil
 }
 
-func (t *defaultModelBuildTask) buildLoadBalancerSecurityGroups(ctx context.Context, listenPortConfigByPort map[int32]listenPortConfig, ipAddressType elbv2model.IPAddressType) ([]core.StringToken, error) {
+func (t *defaultModelBuildTask) buildLoadBalancerSecurityGroups(ctx context.Context, listenPortConfigByPort map[int32]listenPortConfig, ipAddressType elbv2model.IPAddressType, additionalTags map[string]string) ([]core.StringToken, error) {
 	sgNameOrIDsViaAnnotation, err := t.buildFrontendSGNameOrIDsFromAnnotation(ctx)
 	if err != nil {
 		return nil, err
@@ -291,7 +292,7 @@ func (t *defaultModelBuildTask) buildLoadBalancerSecurityGroups(ctx context.Cont
 		if !t.enableBackendSG {
 			t.backendSGIDToken = managedSG.GroupID()
 		} else {
-			backendSGID, err := t.backendSGProvider.Get(ctx, networking.ResourceTypeIngress, k8s.ToSliceOfNamespacedNames(t.ingGroup.Members))
+			backendSGID, err := t.backendSGProvider.Get(ctx, networking.ResourceTypeIngress, k8s.ToSliceOfNamespacedNames(t.ingGroup.Members), additionalTags)
 			if err != nil {
 				return nil, err
 			}
@@ -317,7 +318,7 @@ func (t *defaultModelBuildTask) buildLoadBalancerSecurityGroups(ctx context.Cont
 			if !t.enableBackendSG {
 				return nil, errors.New("backendSG feature is required to manage worker node SG rules when frontendSG manually specified")
 			}
-			backendSGID, err := t.backendSGProvider.Get(ctx, networking.ResourceTypeIngress, k8s.ToSliceOfNamespacedNames(t.ingGroup.Members))
+			backendSGID, err := t.backendSGProvider.Get(ctx, networking.ResourceTypeIngress, k8s.ToSliceOfNamespacedNames(t.ingGroup.Members), additionalTags)
 			if err != nil {
 				return nil, err
 			}

pkg/networking/backend_sg_provider.go
@@ -5,13 +5,14 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
-	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
-	"github.com/aws/smithy-go"
 	"regexp"
 	"sort"
 	"strings"
 	"sync"
 	"time"
+
+	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/aws/smithy-go"
 
 	awssdk "github.com/aws/aws-sdk-go-v2/aws"
 	ec2sdk "github.com/aws/aws-sdk-go-v2/service/ec2"
@@ -52,7 +53,7 @@ const (
 // BackendSGProvider is responsible for providing backend security groups
 type BackendSGProvider interface {
 	// Get returns the backend security group to use
-	Get(ctx context.Context, resourceType ResourceType, activeResources []types.NamespacedName) (string, error)
+	Get(ctx context.Context, resourceType ResourceType, activeResources []types.NamespacedName, additionalTags map[string]string) (string, error)
 	// Release cleans up the auto-generated backend SG if necessary
 	Release(ctx context.Context, resourceType ResourceType, inactiveResources []types.NamespacedName) error
 }
@@ -120,12 +121,12 @@ type defaultBackendSGProvider struct {
 	defaultDeletionTimeout      time.Duration
 }
 
-func (p *defaultBackendSGProvider) Get(ctx context.Context, resourceType ResourceType, activeResources []types.NamespacedName) (string, error) {
+func (p *defaultBackendSGProvider) Get(ctx context.Context, resourceType ResourceType, activeResources []types.NamespacedName, additionalTags map[string]string) (string, error) {
 	if len(p.backendSG) > 0 {
 		return p.backendSG, nil
 	}
 	// Auto generate Backend Security group, and return the id
-	if err := p.allocateBackendSG(ctx, resourceType, activeResources); err != nil {
+	if err := p.allocateBackendSG(ctx, resourceType, activeResources, additionalTags); err != nil {
 		p.logger.Error(err, "Failed to auto-create backend SG")
 		return "", err
 	}
@@ -217,7 +218,7 @@ func (p *defaultBackendSGProvider) existsInObjectMap(resourceType ResourceType, 
 	return false
 }
 
-func (p *defaultBackendSGProvider) allocateBackendSG(ctx context.Context, resourceType ResourceType, activeResources []types.NamespacedName) error {
+func (p *defaultBackendSGProvider) allocateBackendSG(ctx context.Context, resourceType ResourceType, activeResources []types.NamespacedName, additionalTags map[string]string) error {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
 
@@ -241,7 +242,7 @@ func (p *defaultBackendSGProvider) allocateBackendSG(ctx context.Context, resour
 		VpcId:             awssdk.String(p.vpcID),
 		GroupName:         awssdk.String(sgName),
 		Description:       awssdk.String(sgDescription),
-		TagSpecifications: p.buildBackendSGTags(ctx),
+		TagSpecifications: p.buildBackendSGTags(ctx, additionalTags),
 	}
 	p.logger.V(1).Info("creating securityGroup", "name", sgName)
 	resp, err := p.ec2Client.CreateSecurityGroupWithContext(ctx, createReq)
@@ -253,21 +254,30 @@ func (p *defaultBackendSGProvider) allocateBackendSG(ctx context.Context, resour
 	return nil
 }
 
-func (p *defaultBackendSGProvider) buildBackendSGTags(_ context.Context) []ec2types.TagSpecification {
-	var defaultTags []ec2types.Tag
+func (p *defaultBackendSGProvider) buildBackendSGTags(_ context.Context, additionalTags map[string]string) []ec2types.TagSpecification {
+	var tags []ec2types.Tag
 	for key, val := range p.defaultTags {
-		defaultTags = append(defaultTags, ec2types.Tag{
+		tags = append(tags, ec2types.Tag{
 			Key:   awssdk.String(key),
 			Value: awssdk.String(val),
 		})
 	}
-	sort.Slice(defaultTags, func(i, j int) bool {
-		return awssdk.ToString(defaultTags[i].Key) < awssdk.ToString(defaultTags[j].Key)
+
+	for key, val := range additionalTags {
+		tags = append(tags, ec2types.Tag{
+			Key:   awssdk.String(key),
+			Value: awssdk.String(val),
+		})
+	}
+
+	sort.Slice(tags, func(i, j int) bool {
+		return awssdk.ToString(tags[i].Key) < awssdk.ToString(tags[j].Key)
 	})
+
 	return []ec2types.TagSpecification{
 		{
 			ResourceType: resourceTypeSecurityGroup,
-			Tags: append(defaultTags, []ec2types.Tag{
+			Tags: append(tags, []ec2types.Tag{
 				{
 					Key:   awssdk.String(tagKeyK8sCluster),
 					Value: awssdk.String(p.clusterName),

pkg/service/model_build_load_balancer.go
@@ -69,11 +69,11 @@ func (t *defaultModelBuildTask) buildLoadBalancerSpec(ctx context.Context, schem
 	if err != nil {
 		return elbv2model.LoadBalancerSpec{}, err
 	}
-	securityGroups, err := t.buildLoadBalancerSecurityGroups(ctx, existingLB, ipAddressType)
+	tags, err := t.buildLoadBalancerTags(ctx)
 	if err != nil {
 		return elbv2model.LoadBalancerSpec{}, err
 	}
-	tags, err := t.buildLoadBalancerTags(ctx)
+	securityGroups, err := t.buildLoadBalancerSecurityGroups(ctx, existingLB, ipAddressType, tags)
 	if err != nil {
 		return elbv2model.LoadBalancerSpec{}, err
 	}
@@ -111,7 +111,7 @@ func (t *defaultModelBuildTask) buildLoadBalancerSpec(ctx context.Context, schem
 }
 
 func (t *defaultModelBuildTask) buildLoadBalancerSecurityGroups(ctx context.Context, existingLB *elbv2deploy.LoadBalancerWithTags,
-	ipAddressType elbv2model.IPAddressType) ([]core.StringToken, error) {
+	ipAddressType elbv2model.IPAddressType, tags map[string]string) ([]core.StringToken, error) {
 	if existingLB != nil && len(existingLB.LoadBalancer.SecurityGroups) == 0 {
 		return nil, nil
 	}
@@ -133,7 +133,7 @@ func (t *defaultModelBuildTask) buildLoadBalancerSecurityGroups(ctx context.Cont
 		if !t.enableBackendSG {
 			t.backendSGIDToken = managedSG.GroupID()
 		} else {
-			backendSGID, err := t.backendSGProvider.Get(ctx, networking.ResourceTypeService, []types.NamespacedName{k8s.NamespacedName(t.service)})
+			backendSGID, err := t.backendSGProvider.Get(ctx, networking.ResourceTypeService, []types.NamespacedName{k8s.NamespacedName(t.service)}, tags)
 			if err != nil {
 				return nil, err
 			}
@@ -157,7 +157,7 @@ func (t *defaultModelBuildTask) buildLoadBalancerSecurityGroups(ctx context.Cont
 			if !t.enableBackendSG {
 				return nil, errors.New("backendSG feature is required to manage worker node SG rules when frontendSG is manually specified")
 			}
-			backendSGID, err := t.backendSGProvider.Get(ctx, networking.ResourceTypeService, []types.NamespacedName{k8s.NamespacedName(t.service)})
+			backendSGID, err := t.backendSGProvider.Get(ctx, networking.ResourceTypeService, []types.NamespacedName{k8s.NamespacedName(t.service)}, tags)
 			if err != nil {
 				return nil, err
 			}